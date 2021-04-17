d1fba395ec1447e51ef73604894b5053a1349cdc
PF8 <Pedramefaraji@gmail.com>
2021-04-17 10:18:12 +0430

    Added readline for wait

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,8 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello New World!"); 
+            Console.WriteLine("Hello New World!");
+            Console.ReadLine();
         }
     }
 }
